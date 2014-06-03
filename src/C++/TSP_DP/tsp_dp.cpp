15418db959163b5889d5d1c35fb6ee027cda73f9
RazorYhang <709230917@qq.com>
2014-06-03 15:25:58 +0800

    完善了TSP_DP的代码，增加了回溯路径功能

--- src/C++/TSP_DP/tsp_dp.cpp
+++ src/C++/TSP_DP/tsp_dp.cpp
@@ -6,7 +6,7 @@ const int INF = 100;
 int main() {
 
 //测试数据1
-
+/*
     const int nCity = 4;
     const int dCity[nCity][nCity] =
     {   {0,2,5,7},
@@ -14,10 +14,10 @@ int main() {
         {5,8,0,1},
         {7,3,1,0}
     };
-
+*/
 
 //测试数据2
-/*
+
     const int nCity = 6;
     const int dCity[nCity][nCity] =
     {   {0,2,INF,2,1,1},
@@ -27,8 +27,8 @@ int main() {
         {1,1,INF,4,0,3},
         {1,INF,3,INF,3,0}
     };
-*/
-    int c[nCity][1 << nCity];   //DP状态 c[i][j]代表从0开始经过集合j到达i的最短路径
+
+    int c[nCity][1 << nCity-1];   //DP状态 c[i][j]代表从0开始经过集合j到达i的最短路径
 
     for(int i = 0; i <nCity; ++i){  //初始化
         for(int j = 0; j < 1<< nCity; ++j){
@@ -45,10 +45,9 @@ int main() {
     c[0][s-1] = INF;
 
     //填满c[][]
-    for(int j = 1; j < (s-1); ++j){
+    for(int j = 1; j < (s-1); ++j){//（不包括第一个城市）从第二个城市出发，一直到所有城市都被选中而第二个城市未被选中
 
         for(int i = 1; i < nCity; ++i){
-
             //如果城市i不在j集合中，计算从初始点开始经过集合j到i的最短距离
             if(( j & (1 <<(i - 1) ) ) == 0){
 
@@ -63,30 +62,42 @@ int main() {
                             m = tmp;
                         }
                     }
-
                 }
                 c[i][j] = m;
             }
         }
     }
 
-    //判断每一个回路的最小值
+    //选取最小的回路
     c[0][s-1] = INF;
     for(int i = 1; i < nCity; ++i){
         c[0][s-1] = min(c[0][s-1],dCity[0][i] + c[i][(s-1)-(1<<(i-1))]);
     }
 
+/*
     //打印c[][]
     for(int i = 0; i < nCity; i++){
-        for(int j = 0; j < 1<<nCity; j ++){
-            cout<<c[i][j]<<",";
+        for(int j = 0; j < 1 << nCity-1; j ++){
+            cout<<c[i][j]<<", ";
         }
         cout<<endl;
-    }
+    }*/
 
+    cout<<"Minimum travle distance:"<<c[0][s-1]<<endl<<"Routine:";
 
     //输出结果
-    cout<<c[0][s-1]<<endl;
 
+    int lastCity = 0;
+    for( int j = ( 1 << nCity-1 ) - 1; j > 0; ){//设置一个有城市数-1个1的int型蒙版，代表集合j包含除第一个元素以外的所有元素
+        for(int i = 1; i < nCity; ++i){
+            if(c[i][j - (1 << i-1)] + dCity[i][lastCity] == c[lastCity][j]){//
+                lastCity = i;
+                j -= (1 << i-1);
+                cout<<i<<",";
+                break;
+            }
+        }
+    }
 
+    cout<<"0"<<endl;
 }
